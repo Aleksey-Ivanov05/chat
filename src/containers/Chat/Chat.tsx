72105fec4d4ea9abd65697c23f2cf841df5f81b1
Aleksey Ivanov <lepri4dw@gmail.com>
2022-11-21 23:37:40 +0600

    Сделал логику интервала в UseEffect и отрисовки сообщений

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -5,59 +5,34 @@ import {MessageType} from "../../types";
 
 const Chat = () => {
   const [messages, setMessages] = useState<MessageType[]>([]);
-  const url = 'http://146.185.154.90:8000/messages';
-
-  const run = async () => {
-    const response = await fetch(url);
-
-    if (response.ok) {
-      const messages: MessageType[] = await response.json();
-      const newMessages = messages.map(message => ({
-        ...message,
-        datetime: message.datetime.slice(0, -5)
-      }));
-
-      setMessages(newMessages);
-    }
-  }
-
-  useEffect(() => {
-    run().catch(console.error);
-  }, []);
-
 
   useEffect(() => {
     let lastDate = '';
 
     setInterval(async () => {
-      let dateUrl = url;
+      let dateUrl = 'http://146.185.154.90:8000/messages';
 
       if (lastDate) {
         dateUrl += '?datetime=' + lastDate;
       }
 
       const response = await fetch(dateUrl);
-      const messages: MessageType[] = await response.json();
+      const messagesResponse: MessageType[] = await response.json();
 
-      if (messages.length > 0) {
-        lastDate = messages[messages.length - 1].datetime;
-      }
-      const newMessages = messages.map(message => ({
+      const newMessages = messagesResponse.map(message => ({
         ...message,
         datetime: message.datetime.slice(0, -5)
       }));
 
-      console.log('In interval', newMessages);
 
-      for (const message of newMessages) {
+      if (messagesResponse.length > 0) {
+        lastDate = messagesResponse[messagesResponse.length - 1].datetime;
         setMessages(prev => ([
-          ...prev, message
+          ...prev, ...newMessages
         ]));
       }
     }, 3000)
   }, [])
-
-  console.log('Full', messages);
 
   return (
     <>
